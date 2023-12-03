c6d044457f8f181f3b7b42e5129f6a1193d88344
Raul Santirso Granda <rsantirso@gmail.com>
2023-12-03 19:09:24 +0100

    Test passed

--- day03/Gear Ratios/src/main/scala/GearRatios.scala
+++ day03/Gear Ratios/src/main/scala/GearRatios.scala
@@ -10,14 +10,17 @@ import scala.util.control.Breaks.{break, breakable}
   println("################################################################################")
   println("")
 
+  println(s" * Test 01: total sum of engine parts, ${getSumOfEngineParts("input_test.txt")}")
 
+
+def getSumOfEngineParts(inputResource: String): Int =
   var data: Array[Array[Char]] = Array.empty
-  val resource = Source.fromResource("input_test.txt")
+  val resource = Source.fromResource(inputResource)
   for (l <- resource.getLines)
     data :+= l.toCharArray
 
   //I feel better adding this
-  if data.length == 0 || data(0).length == 0 then return
+  if data.length == 0 || data(0).length == 0 then return -1
 
   val rows = data.length
   val cols = data(0).length
@@ -31,38 +34,55 @@ import scala.util.control.Breaks.{break, breakable}
 
     for (c <- 0 until cols)
       if data(r)(c).isDigit then
-        if (isDigit) then
-          number = number * 10 + data(r)(c).toString.toInt
-        else
-          number = data(r)(c).toString.toInt
+        val currentValue = data(r)(c).toString.toInt
+        number =
+          if (isDigit) then number * 10 + currentValue
+          else currentValue
 
         isDigit = true
-        isPartNumber = belongsToPart(data, r, c)
+        isPartNumber = isPartNumber || belongsToPart(data, r, c)
 
       else if isDigit then
         //number ends, add it?
         if isPartNumber then
-          println("Number " + number + " is part of the engine")
           partsSum += number;
         else
           println("Number " + number + " is not part of the engine")
+        end if
 
         isDigit = false
         isPartNumber = false
+      end if
 
-  println("Total sum of parts is " + partsSum)
+  partsSum
 
 
 def belongsToPart(data: Array[Array[Char]], r: Int, c: Int): Boolean =
-  val targetPositions: Array[(Int, Int)] = getSurroundingCoordinates(data.length, data(0).length, r, c)
-  targetPositions.map(getValue(data, _)).exists(isSymbol)
+  getSurroundingCoordinates(r, c)
+    .filter(isInBounds(data,_))
+    .map(getValue(data, _))
+    .exists(isSymbol)
+
+def isInBounds(matrix: Array[Array[Char]], coordinate: (Int, Int)): Boolean =
+  coordinate(0) >= 0
+    && coordinate(0) < matrix.length
+    && coordinate(1) >= 0
+    && coordinate(1) < matrix(0).length
+
+def getSurroundingCoordinates(r: Int, c: Int): Array[(Int, Int)] =
+  var array: Array[(Int, Int)] = Array.empty
+  array :+= (r - 1, c -1)
+  array :+= (r - 1, c)
+  array :+= (r - 1, c + 1)
+  array :+= (r, c - 1)
+  array :+= (r, c + 1)
+  array :+= (r + 1, c - 1)
+  array :+= (r + 1, c)
+  array :+= (r + 1, c + 1)
+  array
 
 def getValue(data: Array[Array[Char]], xy: (Int, Int)): Char =
   data(xy(0))(xy(1))
-
-def getSurroundingCoordinates(totalRows: Int, totalCols: Int, r: Int, c: Int): Array[(Int, Int)] =
-  var array: Array[(Int, Int)] = Array.empty
-  array
 
 def isSymbol(value: Char): Boolean =
   !value.isDigit && value != '.'
