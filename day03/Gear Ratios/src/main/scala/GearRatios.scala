bee84c0bc7beaa2c830df2d7b4f9b6a03d81bb3d
Raul Santirso Granda <rsantirso@gmail.com>
2023-12-03 18:05:10 +0100

    Numbers extracted

--- day03/Gear Ratios/src/main/scala/GearRatios.scala
+++ day03/Gear Ratios/src/main/scala/GearRatios.scala
@@ -1,6 +1,7 @@
 package aoc.gear.ratios
 
 import scala.io.Source
+import scala.util.control.Breaks.{break, breakable}
 
 @main def GearRatios(): Unit =
 
@@ -10,5 +11,35 @@ import scala.io.Source
   println("")
 
 
+  var data = Array.empty[Array[Char]]
   val resource = Source.fromResource("input_test.txt")
-  resource.getLines()
+  for (l <- resource.getLines)
+    data :+= l.toCharArray
+
+  //I feel better adding this
+  if data.length == 0 || data(0).length == 0 then return
+
+  val rows = data.length
+  val cols = data(0).length
+
+  var isDigit = false
+  var number = 0
+  for (r <- 0 until rows)
+
+    for (c <- 0 until cols)
+      if data(r)(c).isDigit then
+        if (isDigit) then
+          number = number * 10 + data(r)(c).toString.toInt
+        else
+          number = data(r)(c).toString.toInt
+
+        isDigit = true
+      else if isDigit then
+        //number ends, add it?
+        println("Number found " + number)
+        isDigit = false
+
+
+
+
+  println("aa")
